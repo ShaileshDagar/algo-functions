6c127bd5018c7175372dfb96fe26a8c514f0b589
Shailesh Dagar <52007469+ShaileshDagar@users.noreply.github.com>
2022-07-05 15:43:51 +0530

    Create merge-three-sorted-arrays.cpp

---
+++ 05-07-2022/merge-three-sorted-arrays.cpp
@@ -0,0 +1,33 @@
+//--- Problem Statement ---
+
+/*Given three sorted arrays A, B and C of size N, M and P respectively.
+The task is to merge them into a single array which must be sorted in increasing order.*/
+
+void mergeTwo(vector<int>& a, vector<int>& b, vector<int>& merged)
+    {
+        int i=0, j=0;
+        int n=a.size();
+        int m=b.size();
+        while(i<n && j<m)
+        {
+            if(a[i]<=b[j])
+            {
+                merged.push_back(a[i]);
+                i++;
+            }
+            else
+            {
+                merged.push_back(b[j]);
+                j++;
+            }
+            if(i==n)
+            {
+                while(j<m)
+                {
+                    merged.push_back(b[j]);
+                    j++;
+                }
+            }
+            else if(j==m)
+            {
+                whi
