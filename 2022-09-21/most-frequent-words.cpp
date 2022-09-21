2bfbcfaeb3e78f7e0c8d007c36f7c19cc3c09ad0
Shailesh Dagar <52007469+ShaileshDagar@users.noreply.github.com>
2022-09-21 20:56:27 +0530

    Update most-frequent-words.cpp

--- 2022-09-21/most-frequent-words.cpp
+++ 2022-09-21/most-frequent-words.cpp
@@ -7,21 +7,58 @@ then print the word whose first occurence occurs last in the array as compared t
 
 //--- Solution ---
 
-string mostFrequentWord(string arr[], int n) 
+class TrieNode
 {
-    // code here
-    pair<string, int> ans;
-    ans.first=arr[n-1];
-    ans.second=1;
-    unordered_map<string, int> m;
-    for(int i=n-1; i>=0; i--)
+    public:
+    bool isend;
+    int count;
+    vector<TrieNode*> child;
+    TrieNode()
     {
-        m[arr[i]]++;
-        if(m[arr[i]]>ans.second)
-        {
-            ans.first =arr[i];
-            ans.second = m[arr[i]];
-        }
+        count=0;
+        isend=false;
+        child.resize(58);
+        for(int i=0; i<58; i++)
+            child[i]=nullptr;
     }
-    return ans.first;
-}
+    int insert(const string& s)
+    {
+        TrieNode* temp = this;
+        int i;
+        for(char x : s)
+        {
+            i = x - 'A';
+            if(temp->child[i] == nullptr)
+                temp->child[i] = new TrieNode();
+            temp = temp->child[i];
+        }
+        (temp->count)++;
+        temp->isend = true;
+        return (temp->count);
+    }
+};
+class Solution
+{
+    public:
+    
+    //Function to find most frequent word in an array of strings.
+    string mostFrequentWord(string arr[], int n) 
+    {
+        // code here
+        TrieNode* t = new TrieNode();
+        pair<string, int> ans;
+        int c;
+        ans.first=arr[n-1];
+        ans.second=1;
+        for(int i=n-1; i>=0; i--)
+        {
+            c = t->insert(arr[i]);
+            if(c>ans.second)
+            {
+                ans.first =arr[i];
+                ans.second = c;
+            }
+        }
+        return ans.first;
+    }
+};
